8c8362546767fa16afdd125422a669666e166fa1
William German <william.j.german2@gmail.com>
2022-11-05 17:23:42 -0700

    commit tree 8952b07129378b089ad0eaf094a8dd53db761f61

---
+++ index.js
@@ -0,0 +1,41 @@
+function receivesAFunction(thisACallback) {
+    thisACallback()
+}
+
+function returnsANamedFunction () {
+    return returnsANamedFunction 
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function receivesAFunction (callback){
+//     callback()
+// }
+// function returnsANamedFunction() {
+//     return returnsANamedFunction
+// }
+
